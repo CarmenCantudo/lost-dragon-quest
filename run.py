7ef55294bea212fd77af61197ad38a6aa28a2337
Carmen <carmencantudo@gmail.com>
2022-08-11 10:01:07 +0000

    Import time and time.sleep to story lines

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import time
+
 
 # Variables
 dragons_name = "Ignit"
@@ -7,16 +9,27 @@ def start_quest():
     Story intro
     """
     print("It was five years ago when, with the shake of the earth upon the eclipse, fire filled the sky.")
+    time.sleep(2)
     print("Mount Bolcán had erupted after 1000 years of sleeping.")
+    time.sleep(2)
     print("They call her the dragon's láir, for when she wakes, the legend says so to a blue back dragon is born.")
+    time.sleep(2)
     print("With a glimmer of light left from the moon and the fire in the sky, an oval-shaped sphere with a bright red shell began to appear.")
+    time.sleep(2)
     print("Tumbling down the mountain until reaching my feet, not a crack nor a scratch to be seen.")
+    time.sleep(2)
     print("Grabbing this egg to shelter in the warmth until that magical seventh night, when, with a crackle quieter than a mouse, I awoke.")
+    time.sleep(2)
     print(f"{dragons_name} was born. From that day until now, we raised {dragons_name} as kin.")
+    time.sleep(2)
     print("That was until Lord Orcus, the evil warlock, took her.")
+    time.sleep(2)
     print("Legend tells that on the blood moon, the blood of a blue back dragon can complete the spell to control all living creatures on planet Apollo.")
+    time.sleep(2)
     print(f"It is but two days before the blood moon, and I must find and save {dragons_name} and all those on Apollo.")
+    time.sleep(2)
     print(f"But all I have is my compass made from the stone of Bolcán, pointing to {dragons_name} and courage to guide me on this perilous quest.")
+    time.sleep(2)
     
     print("")
 
@@ -34,9 +47,12 @@ print("     ######################")
 print()
 print()
 print("Welcome to the Lost Dragon's Quest!")
+time.sleep(2)
 your_name = input("What is your name? \n")
+time.sleep(2)
 print(f"Hello {your_name}!")
 print()
+time.sleep(2)
 startGame = input("Are you ready for an adventure? (Y/N): \n")
 if startGame == "n" or startGame == "N":
     print("Maybe next time")
