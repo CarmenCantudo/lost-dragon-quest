19dd503d957fadf66fbc2d7ad3f44746642694a2
Carmen <carmencantudo@gmail.com>
2022-08-11 09:58:56 +0000

    Add start_quest() function  story intro

--- run.py
+++ run.py
@@ -1,5 +1,28 @@
-# Game intro (From Comp Sci Central: https://youtu.be/ypNFNr72Xe8)
 
+# Variables
+dragons_name = "Ignit"
+
+def start_quest():
+    """
+    Story intro
+    """
+    print("It was five years ago when, with the shake of the earth upon the eclipse, fire filled the sky.")
+    print("Mount Bolcán had erupted after 1000 years of sleeping.")
+    print("They call her the dragon's láir, for when she wakes, the legend says so to a blue back dragon is born.")
+    print("With a glimmer of light left from the moon and the fire in the sky, an oval-shaped sphere with a bright red shell began to appear.")
+    print("Tumbling down the mountain until reaching my feet, not a crack nor a scratch to be seen.")
+    print("Grabbing this egg to shelter in the warmth until that magical seventh night, when, with a crackle quieter than a mouse, I awoke.")
+    print(f"{dragons_name} was born. From that day until now, we raised {dragons_name} as kin.")
+    print("That was until Lord Orcus, the evil warlock, took her.")
+    print("Legend tells that on the blood moon, the blood of a blue back dragon can complete the spell to control all living creatures on planet Apollo.")
+    print(f"It is but two days before the blood moon, and I must find and save {dragons_name} and all those on Apollo.")
+    print(f"But all I have is my compass made from the stone of Bolcán, pointing to {dragons_name} and courage to guide me on this perilous quest.")
+    
+    print("")
+
+
+
+# Game intro (From Comp Sci Central: https://youtu.be/ypNFNr72Xe8)
 print()
 print()
 print("     ######################")
@@ -10,11 +33,11 @@ print("     |                    |")
 print("     ######################")
 print()
 print()
-print("Hello!")
+print("Welcome to the Lost Dragon's Quest!")
 your_name = input("What is your name? \n")
 print(f"Hello {your_name}!")
 print()
-startGame = input("Would you like to go on a quest? (Y/N): \n")
+startGame = input("Are you ready for an adventure? (Y/N): \n")
 if startGame == "n" or startGame == "N":
     print("Maybe next time")
 elif startGame == "y" or startGame == "Y":
