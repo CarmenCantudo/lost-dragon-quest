6b38a1f3b732e7397cbda7abedbbf84499673f0c
Carmen <carmencantudo@gmail.com>
2022-08-11 11:40:51 +0000

    Add # noqa to long string story lines

--- run.py
+++ run.py
@@ -8,27 +8,27 @@ def start_quest():
     """
     Story intro
     """
-    print("It was five years ago when, with the shake of the earth upon the eclipse, fire filled the sky.")
+    print("It was five years ago when, with the shake of the earth upon the eclipse, fire filled the sky.") # noqa
     time.sleep(3)
     print("Mount Bolcán had erupted after 1000 years of sleeping.")
     time.sleep(2)
-    print("They call her the dragon's láir, for when she wakes, the legend says so to a blue back dragon is born.")
+    print("They call her the dragon's láir, for when she wakes, the legend says so to a blue back dragon is born.") # noqa
     time.sleep(3)
-    print("With a glimmer of light left from the moon and the fire in the sky, an oval-shaped sphere with a bright red shell began to appear.")
+    print("With a glimmer of light left from the moon and the fire in the sky, an oval-shaped sphere with a bright red shell began to appear.") # noqa
     time.sleep(3)
-    print("Tumbling down the mountain until reaching my feet, not a crack nor a scratch to be seen.")
+    print("Tumbling down the mountain until reaching my feet, not a crack nor a scratch to be seen.") # noqa
     time.sleep(3)
-    print("Grabbing this egg to shelter in the warmth until that magical seventh night, when, with a crackle quieter than a mouse, I awoke.")
+    print("Grabbing this egg to shelter in the warmth until that magical seventh night, when, with a crackle quieter than a mouse, I awoke.") # noqa
     time.sleep(3)
-    print(f"{dragons_name} was born. From that day until now, we raised {dragons_name} as kin.")
+    print(f"{dragons_name} was born. From that day until now, we raised {dragons_name} as kin.") # noqa
     time.sleep(3)
-    print("That was until Lord Orcus, the evil warlock, took her.")
+    print("That was until Lord Orcus, the evil warlock, took her.")# noqa
     time.sleep(3)
-    print("Legend tells that on the blood moon, the blood of a blue back dragon can complete the spell to control all living creatures on planet Apollo.")
+    print("Legend tells that on the blood moon, the blood of a blue back dragon can complete the spell to control all living creatures on planet Apollo.") # noqa
     time.sleep(3)
-    print(f"It is but two days before the blood moon, and I must find and save {dragons_name} and all those on Apollo.")
+    print(f"It is but two days before the blood moon, and I must find and save {dragons_name} and all those on Apollo.") # noqa
     time.sleep(3)
-    print(f"But all I have is my compass made from the stone of Bolcán, pointing to {dragons_name} and courage to guide me on this perilous quest.")
+    print(f"But all I have is my compass made from the stone of Bolcán, pointing to {dragons_name} and courage to guide me on this perilous quest.") # noqa
     time.sleep(3)
 
     continue_game = input("Will you go to save your dragon now? (Y/N): \n")
@@ -44,25 +44,38 @@ def dark_forest():
     print("  #######################")
     print()
     time.sleep(3)
-    print("The compass is pointing to the North, and to get there you need to go through the Dark Forest.")
+    print("The compass is pointing to the North, and to get there you need to go through the Dark Forest.") # noqa
     time.sleep(3)
-    print("The light fades as you enter the forest, turning into a strange dark and quiet place, the sounds have all but vanished.")
+    print("The light fades as you enter the forest, turning into a strange dark and quiet place, the sounds have all but vanished.") # noqa
     time.sleep(3)
-    print("The smell of animal or beast hangs in a still air, no flowers seem to help the uneasy odor.")
+    print("The smell of animal or beast hangs in a still air, no flowers seem to help the uneasy odor.") # noqa
     time.sleep(3)
-    print("You need to decide if you want to continue walking along the main road or get out.")
+    print("You need to decide if you want to continue walking along the main road or get out.") # noqa
     main_road = input("Do you want to stay in the main road? (Y/N): \n")
+    # Pick an option
     if main_road == "y" or main_road == "N":
-        print("You continue on the main road, walking through the forest, when suddenly you hear a crack.")
-        print("You fall through a hole in the ground and find yourself surronded by goblins.")
+        print("You continue on the main road, walking through the forest, when suddenly you hear a crack.") # noqa
+        time.sleep(3)
+        print("You fall through a hole in the ground and find yourself surronded by goblins.") # noqa
         game_over()
-    elif main_road == "n" or main_road == "N":
+    
+    print("You walk through the dark forest, listening to lots of scary noises.") # noqa
+    time.sleep(3)
+    print("You see some light nearby. The noises come from that area.")
+    time.sleep(3)
+    print("It is a goblin camp. Suddenly a scream cuts the night and everything becomes quiet.") # noqa
+    time.sleep(3)
+    print("The goblins have a man.")
+
         
 
 
 
 
 def game_over():
+    """
+    Finish the game
+    """
     print("GAME OVER")
 
 
